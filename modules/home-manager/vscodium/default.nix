00d1b03be8687846d8fbaf9d501ae1231e49e012
Joshua265 <Joshua_Noel@gmx.de>
2024-03-10 14:16:18 +0100

    20 current  2024-03-10 14:16:13  23.11.20240308.2be119a   6.1.81                          *

--- modules/home-manager/vscodium/default.nix
+++ modules/home-manager/vscodium/default.nix
@@ -1,13 +1,5 @@
 { config, pkgs, lib, ... }:
-let
-  settings-directory =
-    if pkgs.stdenv.hostPlatform.isDarwin
-    then "$HOME/Library/Application Support/Code/User"
-    else "$HOME/.config/Code/User";
-  defaultExtensions = { "remote.SSH.defaultExtensions" = map (x: x.vscodeExtUniqueId) extensions; };
-  userSettings = (builtins.fromJSON (builtins.readFile ./settings.json)) // defaultExtensions;
-  keybindings = builtins.fromJSON (builtins.readFile ./keybindings.json);
-in
+
 {
   programs.vscode = {
     inherit userSettings keybindings;
@@ -45,30 +37,5 @@ in
       tomoki1207.pdf
       twxs.cmake
     ];
-  };
-
-  # Copy VS Code settings into the default location as a mutable copy.
-  home.activation = {
-    beforeCheckLinkTargets = {
-      after = [ ];
-      before = [ "checkLinkTargets" ];
-      data = ''
-        if [ -f "${settings-directory}/settings.json" ]; then
-          rm "${settings-directory}/settings.json"
-        fi
-        if [ -f "${settings-directory}/keybindings.json" ]; then
-          rm "${settings-directory}/keybindings.json"
-        fi
-      '';
-    };
-
-    afterWriteBoundary = {
-      after = [ "writeBoundary" ];
-      before = [ ];
-      data = ''
-        cat ${(pkgs.formats.json {}).generate "settings.json" userSettings} > "${settings-directory}/settings.json"
-        cat ${(pkgs.formats.json {}).generate "keybindings.json" keybindings} > "${settings-directory}/keybindings.json"
-      '';
-    };
   };
 }
