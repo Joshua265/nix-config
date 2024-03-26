4ca4fe42fcb9caea5782d7e714ea2b1d61964e6a
Joshua265 <Joshua_Noel@gmx.de>
2024-03-27 00:03:13 +0100

    136 current  2024-03-27 00:03:09  23.11.20240322.56528ee   6.1.82                          *

--- home-manager/packages.nix
+++ home-manager/packages.nix
@@ -51,21 +51,22 @@
     alejandra
 
     ## python
-    (pkgs.python310.withPackages (python-pkgs: [
-      python-pkgs.pip
-      python-pkgs.pandas
-      python-pkgs.requests
-      python-pkgs.beautifulsoup4
-      python-pkgs.numpy
-      python-pkgs.scipy
-      python-pkgs.matplotlib
-      python-pkgs.pytorch
-      python-pkgs.pydantic
-      python-pkgs.flask
-      python-pkgs.flask-cors
-      python-pkgs.ipykernel
-    ]))
+    # (pkgs.python310.withPackages (python-pkgs: [
+    #   python-pkgs.pip
+    #   python-pkgs.pandas
+    #   python-pkgs.requests
+    #   python-pkgs.beautifulsoup4
+    #   python-pkgs.numpy
+    #   python-pkgs.scipy
+    #   python-pkgs.matplotlib
+    #   python-pkgs.pytorch
+    #   python-pkgs.pydantic
+    #   python-pkgs.flask
+    #   python-pkgs.flask-cors
+    #   python-pkgs.ipykernel
+    # ]))
     ollama
+    conda
 
     ## c
     extra-cmake-modules
