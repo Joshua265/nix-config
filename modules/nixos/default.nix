bc34d19e9cdb5b186cfa2a52bb0877d9a2cec60f
Joshua265 <Joshua_Noel@gmx.de>
2024-10-01 12:57:15 +0200

    import auto-rotate

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -2,6 +2,7 @@
 # These should be stuff you would like to share with others, not your personal configurations.
 {
   # List your module files here
+  auto-rotate = import ./auto-rotate.nix;
   cuda = import ./cuda.nix;
   nvidia = import ./nvidia.nix;
   discord = import ./discord.nix;
