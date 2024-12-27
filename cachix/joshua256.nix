c2a90cdda215421c8056a4ae32bf0876fb654cde
Joshua265 <Joshua_Noel@gmx.de>
2024-12-27 16:50:54 +0100

    update flake lock and add personal cachix

---
+++ cachix/joshua256.nix
@@ -0,0 +1,6 @@
+{
+  nix.settings = {
+    substituters = ["https://joshua256.cachix.org"];
+    trusted-public-keys = ["joshua256.cachix.org-1:0EtYTLjIjFE7IHh6mXY2/sfw+j4XcnarOMZTAgoWagU="];
+  };
+}
