c0c198af929e8fbd3aadc70347da02e141fb0b16
Joshua265 <Joshua_Noel@gmx.de>
2025-10-08 19:29:56 +0200

    desktop 391 current  2025-10-08 19:29:45  25.05.20251006.20c4598  6.12.50                          *

--- flake.nix
+++ flake.nix
@@ -103,7 +103,7 @@
     system = "x86_64-linux";
     # Your custom packages and modifications, exported as overlays
     openglWrappedOverlay = final: prev:
-      prev.lib.genAttrs ["kitty" "alacritty" "blender" "zen-browser" "freecad"]
+      prev.lib.genAttrs ["kitty" "alacritty" "blender" "zen-browser" "freecad" "wasistlos"]
       (name: final.wrapWithNixGLIntel prev.${name});
     overlays = import ./overlays {inherit inputs system;};
     pkgs = import nixpkgs {
