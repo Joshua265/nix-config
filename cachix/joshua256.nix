35817006c8c7ce4af92a7e8dd98828810b77d415
Joshua265 <Joshua_Noel@gmx.de>
2025-10-28 11:02:52 +0100

    framework 48 current  2025-10-28 11:02:50  25.05.20251026.78e34d1  6.12.55                          *

--- cachix/joshua256.nix
+++
@@ -1,6 +0,0 @@
-{
-  nix.settings = {
-    substituters = ["https://joshua256.cachix.org"];
-    trusted-public-keys = ["joshua256.cachix.org-1:0EtYTLjIjFE7IHh6mXY2/sfw+j4XcnarOMZTAgoWagU="];
-  };
-}
---
+++ cachix/joshua265.nix
@@ -0,0 +1,6 @@
+{
+  nix.settings = {
+    substituters = ["https://joshua265.cachix.org"];
+    trusted-public-keys = ["joshua265.cachix.org-1:iWU2ov78zioReN0PQGoYMuMBZG7Dq14JDfrnej/Mabw="];
+  };
+}
